ea5c6b1a3860077d47950db45a365ad576f0e234
Riccardo Moncada <rmondev@gmail.com>
2024-05-29 00:58:16 -0400

    messing with text input

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, {useState} from 'react';
+
 
 export default function App() {
 
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
-      <Text>#rmon.dev Hello</Text>
+      <Text style={styles.textHeader}>Hello World</Text>
+      <Text style={styles.textHeader}>#rmon.dev Hello</Text>
+      <TextInput 
+      placeholder='Enter First Name' 
+      style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 200, margin: 10, padding :8}}
+      defaultValue={firstName}
+      onChangeText={text => setFirstName(text)}
+      />
+      <TextInput 
+      placeholder='Enter Last Name' 
+      style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 200, margin: 10, padding :8}} 
+      defaultValue={lastName}
+      onChangeText={text => setLastName(text)}
+      
+      />
+
+      <Text>First Name: {firstName}</Text>
+      <Text>Last Name: {lastName}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,4 +42,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textHeader:{
+    fontSize: 24,
+    fontWeight: 'bold',
+  }
 });
