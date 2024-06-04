ddbf1879962ddd207730ffe0833c690667f94e8d
Riccardo Moncada <rmondev@gmail.com>
2024-06-03 23:00:30 -0400

    added onboarding page

---
+++ src/app/(days)/day2/onboarding.tsx
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import {Stack} from 'expo-router'
+import { FontAwesome6 } from '@expo/vector-icons';
+
+const OnBoardingScreen = () => {
+  return (
+    <View style={styles.page}>
+        <Stack.Screen options={{headerShown: false}}/>
+        <FontAwesome6 name="people-arrows" size={24} color="#FDFDFD" />
+        <Text style={styles.title}>Track Every Transaction</Text>
+        <Text style={styles.description}>Monitor your spending and contributions, ensuring pennies align with 
+            your families aspirations
+            </Text>
+    </View>
+  )
+}
+
+const styles =StyleSheet.create({
+    page:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#15141A',
+    },
+    title:{
+        color: '#FDFDFD',
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontFamily: 'AmaticBold',
+    },
+    image:{},
+    description:{},
+
+})
+
+export default OnBoardingScreen
